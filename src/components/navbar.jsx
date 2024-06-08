8693a818cfea997df02f9bfaa7592b075b622fdf
Ian Dsouza <Iandsouza03@gmail.com>
2024-06-08 20:33:18 +0100

    Initial commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -27,6 +27,7 @@ function Navigation() {
             sx={{ mr: 2 }}
           >
             <MenuIcon />
+            
           </IconButton>
          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
